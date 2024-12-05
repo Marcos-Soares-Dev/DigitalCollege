8f83904f8f1c2a2a359aa08a50df9b07f19e4e6a
Marcos Soares <messtutors@gmail.com>
2024-12-05 09:02:00 -0300

    atualizando repositorio

---
+++ FS 20/JavaScript/Anotações.js
@@ -0,0 +1,16 @@
+// // para sabermos se um numero é impar ou par podemos utilizar o operador % (resto)
+// // se o resto de um numero for igual a 0 podemos assumir que esse numero é par
+// // caso contrario ele seria impar
+
+// // EXEMPLO:
+// if (numberr % 2 === 0) {
+//     console.log(`O numero ${numberr} é Par`);
+    
+// } else {
+//     console.log(`O numero ${numberr} é Impar`);
+// }
+
+
+// // o operador % tambem pode ser facimente utilizado para verificar se ele é mutiplo de um numero
+
+// // EXEMPLO?
---
+++ FS 20/JavaScript/If Else/index.js
@@ -0,0 +1,41 @@
+// const number1 = Number(prompt("informe o numero 1"))
+// const number2 = Number(prompt("informe o numero 2"))
+
+// if (number1 > number2) {
+//     alert("O numero 1 é maior que o numero 2")
+// } else if (number2 > number1) {
+//     alert("O numero 2 é maior que o numero 1")
+// } else {
+//     alert("Os numeros sao iguais")
+// }
+
+// const userNumber = Number(prompt(`Informe um numero para checar se ele é maior ou menor que 10`))
+
+// if (userNumber > 10) {
+//     alert(`O numero: ${userNumber}
+//     é MAIOR que 10`)
+// } else {
+//     alert(`O numero: ${userNumber}
+//     é MENOR que 10`)
+// }
+
+
+
+// Verificar se é A
+// const userLetter =  prompt("informe uma letra").toLowerCase()
+
+// if (userLetter == "a") {
+//     alert("A letra digitada foi A")
+// } else {
+//     alert("Letra errada, tente novamente")
+// }
+
+// Verificar se é maior de idade
+
+// const ageBorn = Number(prompt(`Informe seu ano de nascimento?`))
+
+// if (2024 - ageBorn >= 18) {
+//     alert(`Você tem ${2024 - ageBorn}, e é maior de idade`)
+// } else {
+//     alert(`Você tem ${2024 - ageBorn}, e é menor de idade`)
+// }
